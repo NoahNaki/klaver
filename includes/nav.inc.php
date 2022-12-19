79e47b8f501eafba82350b13e93647211272fde1
fbi <brandinios@pm.me>
2022-12-19 13:42:21 +0100

    Base template

---
+++ includes/nav.inc.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li><a href="index.php">home</a></li>
+        <li>
+            <span>
+                test
+            </span>
+            <ul>
+                <!-- drop down content -->
+            </ul>
+        </li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/stylesheets/style.css">
+    <title>Title</title>
+</head>
+<body>
+<?php
+/**
+ * Includes the navbar for better code re-usability
+ *
+ */
+include_once 'includes/nav.inc.php'
+?>
+<main>
+    <section>
+        <h1>Welkom</h1>
+    </section>
+
+</main>
+</body>
+</html>
