8b59497d083dec1048d89c1d55458be7d2b77a82
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2022-12-20 14:06:13 +0100

    vacancies show

---
+++ vacancies.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mysql1234";
+$dbname = "klaver";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `vacancies`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["type"]. " - Name: ". $row["company"]. " " . $row["company"] . " - Time: ". $row["placedate"]. " - education: ". $row["education"]. " - Time: ". $row["time"]. " - Salary: ". $row["salary"];
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
+</body>
+</html>
