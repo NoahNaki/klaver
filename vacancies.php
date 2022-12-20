231d2d65290a4dc0366a9223817383de1e50f7f1
NoahNaki <74353773+NoahNaki@users.noreply.github.com>
2022-12-20 14:26:21 +0100

    table

--- vacancies.php
+++ vacancies.php
@@ -15,14 +15,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM `vacancies`";
+$sql = "SELECT type, company, category, placedate, education, time, salary  FROM vacancies";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo "<table><tr><th>type</th><th>company</th><th>category</th><th>placedate</th><th>education</th><th>time</th><th>salary</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["type"]. " - Name: ". $row["company"]. " " . $row["company"] . " - Time: ". $row["placedate"]. " - education: ". $row["education"]. " - Time: ". $row["time"]. " - Salary: ". $row["salary"];
+        echo "<tr><td>" . $row["type"]. "</td><td>" . $row["company"]. "</td><td>" . $row["category"]. "</td><td>" . $row["placedate"]. "</td><td>" . $row["education"]. "</td><td>" . $row["time"]. "</td><td>" . $row["salary"]. "</td><td>";
     }
+    echo "</table>";
 } else {
     echo "0 results";
 }
