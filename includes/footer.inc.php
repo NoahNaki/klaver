436bd491e82e37be407cefbb72baadcd5f2f113d
sketje <sketje0722@hotmail.com>
2022-12-21 13:25:15 +0100

    Kleine aanpassing

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,14 +1,20 @@
 <footer>
   <div>
     
-  <p><a href="#">Home</a>  </p>
-  <p><a href="#">Voorlichting</a></p>
-  <p><a href="#">voorwaarden</a>  </p>
-  <p><a href="#">Vacatures</a></p>
-  <p><a href="#">Prikbord</a></p>
-  <p><a href="#">Chat</a></p>
-  <p><a href="#">Login</a></p>
+
+    <nav>
+      <ul>
+    
+  <li><a href="#">Home</a>  </li>
+  <li><a href="#">Voorlichting</a></li>
+  <li><a href="#">voorwaarden</a>  </li>
+  <li><a href="#">Vacatures</a></li>
+  <li><a href="#">Prikbord</a></li>
+  <li><a href="#">Chat</a></li>
+  <li><a href="#">Login</a></li>
       <p><a href="#">Registreren</a></p>
+</ul> 
+      </nav>
     <p> &copy; <span id="current-year">2022</span> XXL </p>
   </div>
 </footer>
