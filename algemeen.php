e747f08a5ffb7cfd69c6f3e0800bf500b208815b
sketje <sketje0722@hotmail.com>
2022-12-21 11:06:31 +0100

    Simple footer

---
+++ algemeen.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/stylesheets/style.css">
+    <title>Title</title>
+</head>
+<body>
+<?php
+/**
+ * Includes the navbar for better code re-usability
+ *
+ */
+include_once 'includes/nav.inc.php'
+?>
+<main>
+    <section>
+        <br>
+        <h1>Algemenen voorwaarden</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus vehicula odio, nec ornare ante mattis sed. Nam pharetra turpis erat. Curabitur ut bibendum nulla. Vivamus dictum interdum lacus, sed molestie neque laoreet quis. Sed dignissim eu nulla quis semper. Donec euismod, nisi et tempor porta, augue mauris vehicula ex, at aliquam felis mauris ac quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse vulputate, dolor eu lacinia pretium, nibh justo egestas lacus, nec malesuada sem metus at risus.
+        </p>
+        <p> Donec dignissim massa rhoncus metus eleifend accumsan vitae sit amet risus. Morbi condimentum enim urna, nec blandit ex elementum vitae. Praesent scelerisque, odio ac imperdiet dignissim, dui tortor condimentum lorem, vitae congue libero tellus nec sapien. Donec luctus tellus in placerat consectetur. Proin nunc turpis, lobortis in hendrerit in, faucibus ornare nisl. Proin malesuada non lectus non eleifend. Donec ex orci, pretium sit amet dolor et, dignissim commodo augue. Maecenas nec neque aliquet, feugiat turpis rhoncus, pharetra tortor.</p>
+    </section>
+</main>
+<?php
+include_once 'includes/footer.inc.php'
+?>
+</body>
+</html>
---
+++ includes/footer.inc.php
@@ -0,0 +1,15 @@
+<footer>
+  <div>
+    <ul>
+      <li><a href="#">Home</a>  
+      <a href="#">Voorlichting</a></li>
+      <li><a href="#">voorwaarden</a>  </li>
+      <li> <a href="#">Vacatures</a></li>
+      <li><a href="#">Prikbord</a></li>
+      <li><a href="#">Chat</a></li>
+      <li><a href="#">Login</a></li>
+      <li><a href="#">Registreren</a></li>
+    </ul>
+    <p> &copy; <span id="current-year">2022</span> XXL </p>
+  </div>
+</footer>
