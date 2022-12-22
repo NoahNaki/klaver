0b6008c2b0d17e083f70e74298c42c1d8a6a97f4
sketje <sketje0722@hotmail.com>
2022-12-22 11:13:51 +0100

    sui

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-/**
- * The main page of our website
- *
- */
-
-session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,21 +16,8 @@ include_once 'includes/nav.inc.php'
 <main>
     <section>
         <br>
-        <h1>
-            <?php
-            if (isset($_SESSION['id'])) {
-                echo 'Welkom, '. $_SESSION['userName'] . '!';
-            } else {
-                echo 'Welkom!';
-            }
-            ?>
-        </h1>
+        <h1>Welkom fijne mijheer/mevrouw</h1>
     </section>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, </p>
 </main>
 <?php
 include_once 'includes/footer.inc.php'
